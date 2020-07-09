9ef3d97127d88b57ce4a26006b81b3f89692abad
Yijian Hu <yijianhu@asu.edu>
2020-07-08 18:52:02 -0700

    Update wordCloud.js

--- scripts/wordCloud.js
+++ scripts/wordCloud.js
@@ -20,8 +20,9 @@ function WordCloud(data, state)
     // append the svg object to the body of the page
     var svg = d3.select("#wordcloud")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+        .attr("height", height + margin.top + margin.bottom);
+    var g = svg
+        .append("g")
         .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
     // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
@@ -29,7 +30,7 @@ function WordCloud(data, state)
     var layout = d3.layout.cloud()
     .size([width, height])
     .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
-    .padding(5)        //space between words
+    .padding(7)        //space between words
     .rotate(function() { return ~~(Math.random() * 2) * 90; })
     .fontSize(function(d) { return d.size; })      // font size of words
     .on("end", draw);
@@ -55,7 +56,7 @@ function WordCloud(data, state)
                       .domain([d3.min(myWords,d=>d.size),d3.max(myWords,d=>d.size)])
                       .range([20,78]);
         layout.words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
-            .padding(5)        //space between words
+            .padding(7)        //space between words
             .rotate(function() { return ~~(Math.random() * 2) * 90; })
             .fontSize(function(d) { return scale(d.size); })      // font size of words
             .on("end", draw)
@@ -70,22 +71,52 @@ function WordCloud(data, state)
     var alphaScale = d3.scaleLinear()
                         .domain([20,78])
                         .range([0.5,1]);
-    svg
-        .selectAll("g").remove();
-    svg
-        .append("g")
-        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-        .selectAll("text")
-            .data(words)
+    // svg
+    //     .selectAll("g").remove();
+    // var updateWord = g.attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    //                     .selectAll('text').data(words);
+    g.attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")");
+    var text = g.selectAll("text").data(words,d=>d.text);
+    text.attr("class","update")
+        .transition().duration(d=>1000*alphaScale(parseInt(d.size)))
+        .style("font-size", function(d) { return d.size+"px"; })//.transition().duration(d=>100*alphaScale(d.size))
+        .style("fill", "#69b3a2")
+        .transition().duration(d=>2000*alphaScale(d.size))
+        .style('opacity',d=>alphaScale(d.size))
+        .attr("text-anchor", "middle")
+        .style("font-family", "Impact")
+        .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; })
+        ;
+
+    svg.selectAll(".exit").remove();
+    text.exit()
+        .attr("class","exit")
+        .selectAll()
+        .style("fill","black")
+        .transition().duration(1000);
+    
+    //text.selectAll(".enter").remove();
+    text
         .enter().append("text")
-            .style("font-size", function(d) { return d.size; })
-            .style("fill", "#69b3a2")
+            .attr("class","enter")
+            .style("fill", "60DA3F")
+            .transition()
+            .style("font-size", function(d) { return d.size+"px"; })
+            .duration(d=>1000/alphaScale(d.size))
             .style('opacity',d=>alphaScale(d.size))
             .attr("text-anchor", "middle")
             .style("font-family", "Impact")
             .attr("transform", function(d) {
             return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
             })
-            .text(function(d) { return d.text; });
+            .text(function(d) { return d.text; })
+            ;
+    svg.selectAll(".exit").transition().duration(1000).style("fill", 'gray');
+    
+    
+    
     }
 }
